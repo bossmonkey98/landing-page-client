1516cecc15c68fd45bac6f9daf1931a973dd5e56
bossmonkey98 <arunambregol007@gmail.com>
2023-12-07 13:25:10 +0530

    [feat] made responsive

--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -1,22 +1,40 @@
-import React from 'react'
+import {useState} from 'react'
 import "./Auth.css"
 
 export const Auth = () => {
-    
-    const handleSubmit = (e) =>{
-        e.preventDefault()
-        window.location.href = 'https://app.loch.one/welcome';
+  const [email, setEmail] = useState('');
+  const [isValid, setIsValid] = useState(true);
+
+  const validateEmail = () => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+
+    setIsValid(isValidEmail);
+    return isValidEmail;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isEmailValid = validateEmail();
+    if (isEmailValid) {
+      window.location.href = 'https://app.loch.one/welcome';
     }
+  };
+
+    
 
   return (
-    <div className='authContainer'>
-        <form className='authForm' onSubmit={handleSubmit}>
+    <div className='authContainer' >
+        <form className='authForm' onSubmit={handleSubmit} noValidate>
             <h3 className='authHeader'>Sign up for exclusive access.</h3>
             <input 
             type='email' 
             placeholder='Your email address here'
             className='textfield'
-            onChange={null}/>
+            onChange={(e)=>setEmail(e.target.value)}
+            
+            />
+            {!isValid && <p style={{color:"red"}}>Please enter a valid email address.</p>}
             <button type='submit' className='submitBtn'>Get Started</button>
             <p className='text'>You'll receive an email with an invite link to join.</p>
         </form>
--- src/Components/SideBlock/Sideblock.jsx
+++ src/Components/SideBlock/Sideblock.jsx
@@ -4,8 +4,11 @@ import {TestCard} from "../TestCard/TestCard"
 import logo from '../../assets/Vector.png'
 import cohort from '../../assets/Cohorts 1.png'
 import bell from '../../assets/Bell.png'
+import mask from '../../assets/Mask group.png'
+import eye from '../../assets/Eye.png'
 
 export const Sideblock = () => {
+  
   const testimonials = [{
     name:"Jack F",
     role:"Ex Blackrock PM",
@@ -30,7 +33,7 @@ export const Sideblock = () => {
                         <img
                           src={bell}
                           className="img"
-                          alt=""
+                          alt="Bell Icon"
                         />
                         <h4 className='h4-1' >
                           Get notified when a highly correlated whale makes a
@@ -45,8 +48,7 @@ export const Sideblock = () => {
                     </div>
                     <div className="column-3">
                       <img
-                        loading="lazy"
-                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/a1ce05073d40ccac3b03444208211c1a002eba6284ae65837aa73c8c6523a470?apiKey=381fc8579b114fc6a0a6eb93cf5631cc&"
+                        src={mask}
                         className="img-2"
                         alt=""
                       />
@@ -54,10 +56,10 @@ export const Sideblock = () => {
                   </div>
         </section>
         <section className='contentContainer'>
-          <div>
+         
             <img src={cohort} alt="" />
-          </div>
           <div className='text-1'>
+            <img className='img-3' src={eye} alt='Eye Icon'/>
             <h4 className='h4-2'>Watch what the whales are doing</h4>
             <p className='p-2'>All whales are not equal. Know exactly what the whales impacting YOUR portfolio are doing.</p>
           </div>
